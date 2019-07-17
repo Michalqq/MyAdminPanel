a7d6420b28b5f644f8aa2ee4e565e2db206135cf
Michalqq <kraciukmichal@gmail.com>
2019-07-17 15:54:08 +0200

    first commit

---
+++ src/main/java/myapp/MyAdminPanel/MyAdminPanelApplication.java
@@ -0,0 +1,13 @@
+package myapp.MyAdminPanel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAdminPanelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAdminPanelApplication.class, args);
+	}
+
+}
---
+++ src/main/java/myapp/MyAdminPanel/controller/HomeController.java
@@ -0,0 +1,14 @@
+package myapp.MyAdminPanel.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String getHome(ModelMap modelMap){
+        return "index";
+    }
+}
