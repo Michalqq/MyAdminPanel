0366026b98f6b676aac6a26f0d60ce0fcf1e8c5c
Michalqq <kraciukmichal@gmail.com>
2019-08-12 15:57:32 +0200

    edit, pobrania

--- src/main/resources/static/js/statusInTable.js
+++ src/main/resources/static/js/statusInTable.js
@@ -2,7 +2,7 @@ function checkStatus(){
     mytab = document.getElementById("mainTable");
     for (let row of mytab.rows)
     {
-        if (row.cells.item(10).innerHTML == "") row.cells.item(10).innerHTML = "W transporcie"
+        if (row.cells.item(10).innerHTML == "") row.cells.item(10).innerHTML = "W transp"
         if (row.cells.item(10).innerHTML == 1) row.cells.item(10).innerHTML = "W domu"
         if (row.cells.item(10).innerHTML == 2){
          row.cells.item(10).innerHTML = "Pobranie"
