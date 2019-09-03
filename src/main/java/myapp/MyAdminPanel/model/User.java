061eac6933a618b2edb993812c3875ca1070aa39
Michalqq <kraciukmichal@gmail.com>
2019-09-03 15:06:51 +0200

    21

--- src/main/java/myapp/MyAdminPanel/model/User.java
+++ src/main/java/myapp/MyAdminPanel/model/User.java
@@ -34,7 +34,11 @@ public class User {
     private String lastName;
     @Column(name = "active")
     private int active;
-    @ManyToMany(cascade = CascadeType.ALL)
+    //@ManyToMany(cascade = CascadeType.ALL)
+    @ManyToMany(cascade = {
+            CascadeType.PERSIST,
+            CascadeType.MERGE
+    })
     @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;
 
