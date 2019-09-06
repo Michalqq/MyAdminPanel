d86dd5eaeca0054856a9be3682e923bef21e7176
Michalqq <kraciukmichal@gmail.com>
2019-09-06 15:15:11 +0200

    ref

--- src/main/java/myapp/MyAdminPanel/controller/ReportController.java
+++ src/main/java/myapp/MyAdminPanel/controller/ReportController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,11 +42,21 @@ public class ReportController {
     }
 
     public List<Double> getProfitSum(List<Item> items, String startDate, String stopDate, ModelAndView modelAndView) {
+        if (startDate.equals("")) startDate = this.getYesterDay();
+        if (stopDate.equals("")) stopDate = this.getToday();
         List<Double> myItems = new ArrayList<>();
         for (Item item : items) {
             List<MyItem> myItemsTemp = myItemRepository.findAllItemsWhereSellDateBetween(startDate, stopDate, item.getId());
-            myItems.add(myItemsTemp.stream().mapToDouble(x->x.getSellPrice()).sum() - myItemsTemp.stream().mapToDouble(x->x.getBuyPrice()).sum());
+            myItems.add(myItemsTemp.stream().mapToDouble(x -> x.getSellPrice()).sum() - myItemsTemp.stream().mapToDouble(x -> x.getBuyPrice()).sum());
         }
         return myItems;
+    }
+
+    private String getYesterDay() {
+        return DateTimeFormatter.ofPattern("yyy-MM-dd").format(LocalDateTime.now().minusDays(1));
+    }
+
+    private String getToday() {
+        return DateTimeFormatter.ofPattern("yyy-MM-dd").format(LocalDateTime.now());
     }
 }
