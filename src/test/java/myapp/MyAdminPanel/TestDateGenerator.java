0543e19085d34db048e7fa94ee278bd38a8b1549
michalqq <kraciukmichal@gmail.com>
2019-09-11 19:43:45 +0200

    junit test

---
+++ src/test/java/myapp/MyAdminPanel/TestDateGenerator.java
@@ -0,0 +1,20 @@
+package myapp.MyAdminPanel;
+
+import myapp.MyAdminPanel.service.DateGenerator;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestDateGenerator {
+
+
+    @Test
+    public void testGetLastDateOfMonth() {
+        assertEquals(LocalDate.now().getYear() + "-SEPTEMBER-01", DateGenerator.getFirstDayOfMonth("SEPTEMBER"));
+        assertEquals(LocalDate.now().getYear() + "-JANUARY-01", DateGenerator.getFirstDayOfMonth("JANUARY"));
+    }
+
+}
