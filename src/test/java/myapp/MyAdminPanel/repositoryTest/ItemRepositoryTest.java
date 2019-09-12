a3fafcddcd59f5945aa1b5f9427c2cc398ea8a5f
Michalqq <kraciukmichal@gmail.com>
2019-09-12 11:08:39 +0200

    itemTestJpa

--- src/test/java/myapp/MyAdminPanel/repositoryTest/ItemRepositoryTest.java
+++ src/test/java/myapp/MyAdminPanel/repositoryTest/ItemRepositoryTest.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
@@ -29,9 +31,24 @@ public class ItemRepositoryTest {
         entityManager.flush();
 
         Item found = itemRepository.findByName(item.getName());
-
         assertThat(found.getName()).isEqualTo(item.getName());
 
+    }
+    @Test
+    public void findByIdLessThanTest() {
+        Item item = new Item();
+        Item item2 = new Item();
+        item.setName("test");
+        item2.setName("test2");
+        entityManager.persist(item);
+        entityManager.persist(item2);
+        entityManager.flush();
+
+        List<Item> found = itemRepository.findByIdLessThan(item2.getId());
+        List<Item> found2 = itemRepository.findByIdLessThan(item2.getId()+1);
+
+        assertThat(found.size()).isEqualTo(1);
+        assertThat(found2.size()).isEqualTo(2);
     }
 
 }
