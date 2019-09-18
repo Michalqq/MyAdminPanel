c8d66cbd21acc84fc04a14d1d7158bced317bbcd
Michalqq <kraciukmichal@gmail.com>
2019-09-18 09:44:20 +0200

    dataTableSort

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -32,6 +32,13 @@ function iterateTableGetChecked(){
     }
 }
 
+$(document).ready( function () {
+    $('#table_id').DataTable();
+} );
+window.$ = window.jquery = require('./node_modules/jquery');
+window.dt = require('./node_modules/datatables.net')();
+window.$('#table_id').DataTable();
+
 (function ($) {
   // USE STRICT
   "use strict";
