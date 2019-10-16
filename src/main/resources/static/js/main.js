664ebabe0033de7e6d3fe2dcabab706ccce0a99e
michalqq <kraciukmichal@gmail.com>
2019-10-16 20:52:20 +0200

    totalprofit

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -292,6 +292,7 @@ function currencyLoader(){
               display: true,
               ticks: {
                 beginAtZero: true,
+                display: false,
               }
             }]
           }
