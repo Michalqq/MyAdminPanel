4b2dcd9b4ccfd7c05301d3032a9a9334dd4e66c1
Michalqq <kraciukmichal@gmail.com>
2019-10-24 11:54:22 +0200

    item history added

---
+++ src/main/java/myapp/MyAdminPanel/controller/ItemHistoryController.java
@@ -0,0 +1,29 @@
+package myapp.MyAdminPanel.controller;
+
+import myapp.MyAdminPanel.model.Basket;
+import myapp.MyAdminPanel.repository.MyItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+public class ItemHistoryController {
+
+    private MyItemRepository myItemRepository;
+
+    private Basket basket;
+
+    @Autowired
+    public ItemHistoryController(MyItemRepository myItemRepository, Basket basket){
+        this.myItemRepository = myItemRepository;
+        this.basket = basket;
+    }
+
+    @RequestMapping(value = {"/sell"}, params = "itemHistory", method = RequestMethod.POST)
+    public ModelAndView addToBasket(@RequestParam(name = "checkedItemId") int itemId,
+                                    ModelAndView modelAndView) {
+        modelAndView.setViewName("itemhistory");
+        return modelAndView;
+    }
+}
